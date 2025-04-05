cca96df60eb5ad6b50e314588ae811da325693da
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2025-04-06 05:06:17 +0530

    feat : make top title bar component and in +layout.svelte

---
+++ frontend/pwa-oauth/src/lib/components/TopBar.svelte
@@ -0,0 +1,12 @@
+<div>
+    <nav class="sticky top-0 z-10 bg-[#1E1E1E] shadow-md px-4 py-3 flex justify-between items-center border-b border-[#333333]">
+        <div class="flex items-center gap-2">
+          <img 
+          src="/ARC.png" 
+          alt="Robokit Logo" 
+          class="h-12 w-12 rounded-full object-cover"
+        />
+          <h1 class="font-bold text-3xl text-white">Robokit</h1>
+        </div>
+      </nav>
+</div>
--- frontend/pwa-oauth/src/routes/+layout.svelte
+++ frontend/pwa-oauth/src/routes/+layout.svelte
@@ -2,8 +2,13 @@
     import { page } from "$app/stores";
     import "../app.css";
     import NavBar from "$lib/components/NavBar.svelte";
-    $: showNavBar = $page.url.pathname !== '/';
+    import TopBar from "$lib/components/TopBar.svelte";
+    $: showNavBar = $page.url.pathname !== '/unauthorized' || $page.url.pathname === '/';
+    $: showTopBar = $page.url.pathname !== '/';
 </script>
+{#if showTopBar}
+<TopBar />
+{/if}
  <slot />
 {#if showNavBar}
 <NavBar />
